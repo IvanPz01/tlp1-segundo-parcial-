d81bf582d2402a687f2b6e82986a6933d5aef088
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-06-21 14:54:18 -0300

    se completo la importaciones, se agragaron los Middlewares y la peticion si no coinciden con ninguna ruta declarada, por ultimo se creo una variable de entorno para el port

---
+++ server/app.js
@@ -0,0 +1,46 @@
+// Imports
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+require("dotenv").config();
+const port = process.env.PORT || 6000;
+
+const app = express();
+
+// Middlewares
+// TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/api", require("./routes/reserva.routes"));
+
+// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+  res.write(`<div>
+          <h1>404 - Ruta no encontrada</h1>
+          <hr>
+          <p>La pagina que intentas buscar no existe</p>
+          <p>Redireccionando a la página de inicio...</p>
+          <script>
+          (
+            () => setTimeout(() => {
+              window.location.href='http://localhost:${port}/tareas';
+             }, 3000)           
+          )();
+          </script>
+      </h1>`);
+});
+
+// Starting the server
+app.listen(port, () =>
+  console.log(`Servidor corriendo en http://localhost:${port}`)
+);
