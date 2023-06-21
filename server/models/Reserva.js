826a3dd2f5dbf0bf67101ebf2ee1cf2e095457d2
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-06-21 15:18:08 -0300

    Se creo el modelo del cliente para la reserva

--- server/models/Reserva.js
+++ server/models/Reserva.js
@@ -1,1 +1,48 @@
 // TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require("../db");
+
+const Cliente = sequelize.define(
+  "Cliente",
+  {
+    nombreYApellido: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    codeReser: {
+      type: DataTypes.INET,
+      allowNull: false,
+      unique: {
+        args: true,
+        messge: "El codigo de reserva ya existe",
+      },
+    },
+    Fecha: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: "Cliente",
+  }
+);
+
+Cliente.sync();
+
+module.exports = Cliente;
